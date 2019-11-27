6c2bc5e420979b7912d856b8bb2a4884819d62ad
nosoded <33593191+nosoded@users.noreply.github.com>
2019-11-27 21:57:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,45 +1,38 @@
-from bs4 import BeautifulSoup
 import requests
-headers = {'accept': '*/*',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
+import time
+from bs4 import BeautifulSoup
+import re
+import time
+def get_tor_session():
+    session = requests.session()
+    # Tor uses the 9050 port as the default socks port
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
+    return session
 
-base_url = 'https://steamcommunity.com/market/search?appid=730&q=#p1'
-def ss_parse(base_url, headers):
-    urls = []
-    urls.append(base_url)
-    session = requests.Session()
-    request = session.get(base_url, headers=headers)
+session = get_tor_session()
+request = session.get("https://steamcommunity.com/market/search?appid=730&q=#p0")
 
-    if request.status_code == 200:
-        soup = BeautifulSoup(request.content, 'html.parser')
-        try:
-            count = int(1301)
-            for i in range(count):
-                url = f'https://steamcommunity.com/market/search?appid=730&q=#p{i}'
-                if url not in urls:
-                    urls.append(url)
-        except:
-            pass
-        urlparse = soup.find_all('div', attrs={'id': 'searchResultsRows'})
-        firstpriceparse = soup.find_all('span', attrs={'class': 'market_table_value normal_price'})
-        for fp in firstpriceparse:
-            firstprice = fp.find('span', attrs={'class': 'normal_price'})['data-price']
-            #print(int(firstprice)/100)
+def sc_parse():
+    soup = BeautifulSoup(request.content, 'html5lib')
+    urlparse = soup.find_all('div', attrs={'id': 'searchResultsRows'})
+    for parseuk in urlparse:
+        hrefUK = parseuk.find_all('a', attrs={'class': 'market_listing_row_link'})
+        for a in hrefUK:
+            z = a["href"]
+            print("var z = ", z)
+            somebody = session.get(z)
+            souup = BeautifulSoup(somebody.content, 'html5lib')
 
-        for parseuk in urlparse:
-            hrefUK = parseuk.find_all('a', attrs={'class': 'market_listing_row_link'})
-            for a in hrefUK:
-                z = a["href"]
-                somebody = session.get(z, headers = headers)
-                souup = BeautifulSoup(somebody.content, 'html.parser')
-                sightparse = souup.find_all('div', attrs={'id': 'market_commodity_buyrequests'})
-                print(sightparse)
-                for lolo in sightparse:
-                    try:
-                        itemname = lolo.find_all('span', attrs={'class': 'market_commodity_orders_header_promote'}).text
-                        print(itemname)
-                    except: pass
-    else:
-        print("error")
-ss_parse(base_url,headers)
-#https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Redline%20%28Field-Tested%29
+            sightparse = souup.find_all('script', attrs={'type': 'text/javascript'})
+            try:
+                result = re.search('ItemActivityTicker.Start\((.*)\)', sightparse.__str__())
+                itemId = result.group(1)
+            except:
+                result = re.search('Market_LoadOrderSpread\((.*)\)', sightparse.__str__())
+                itemId = result.group(1)
+            print(itemId)
+            print(request.status_code)
+            print("------------------------------------------------------------------------------------")
+            time.sleep(0.3)
+sc_parse()
